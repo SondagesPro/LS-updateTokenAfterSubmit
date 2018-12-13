6788525787d97a9c9dc16bbc657e04fb7f6df0bb
Denis Chenu <denis@sondages.pro>
2018-12-13 16:24:47 +0100

    [feature] Automatic (by default) for reset token and response [fix] Disable automatically survey when it can't really be used [feature] Allow redirect inside plugin settings

--- updateTokenAfterSubmit.php
+++ updateTokenAfterSubmit.php
@@ -5,7 +5,7 @@
  * @author Denis Chenu <denis@sondages.pro>
  * @copyright 2018 Denis Chenu <http://www.sondages.pro>
  * @license GPL v3
- * @version 0.2.0
+ * @version 0.3.2
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE as published by
@@ -25,17 +25,41 @@ class updateTokenAfterSubmit extends PluginBase {
     protected $storage = 'DbStorage';
 
     public function init() {
-        $this->subscribe('afterSurveyComplete','updateToken');
-        $this->subscribe('afterSurveyQuota','updateToken');
+        $this->subscribe('afterSurveyComplete','afterSurveyComplete');
+        $this->subscribe('afterSurveyQuota','afterSurveyQuota');
         /* Survey settings */
         $this->subscribe('beforeSurveySettings');
         $this->subscribe('newSurveySettings');
     }
 
+    public function afterSurveyComplete() {
+        if($this->updateToken()) {
+            if($this->get('autoRedirect','Survey',$surveyId,1)) {
+                $surveyId = $this->getEvent()->get('surveyId');
+                $language = Yii::app()->getLanguage();
+                $oSurveyLanguageSettings = SurveyLanguageSetting::model()->findByPk(array('surveyls_survey_id'=>$surveyId, 'surveyls_language'=>$language));
+                if(!empty($oSurveyLanguageSettings) && !empty($oSurveyLanguageSettings->surveyls_url)) {
+                    header("Location: {$oSurveyLanguageSettings->surveyls_url}");
+                }
+            }
+        }
+    }
+
+    public function afterSurveyQuota() {
+        $this->updateToken();
+    }
+
+    /**
+     * update token and response
+     * @return boolean if action is done (with or without success)
+     */
     public function updateToken() {
         $surveyId = $this->getEvent()->get('surveyId');
         $responseId = $this->getEvent()->get('responseId');
         if(empty($responseId)) {
+            return;
+        }
+        if(!$this->_surveySettingsAllowUsage($surveyId)) {
             return;
         }
         $fixedCode = $this->get('fixedCode','Survey',$surveyId,"");
@@ -51,10 +75,23 @@ class updateTokenAfterSubmit extends PluginBase {
         if(!$hasTokenTable) {
             return;
         }
-        $token = isset($_SESSION['survey_'.$surveyId]['token']) ? $_SESSION['survey_'.$surveyId]['token']: null;
-        if(empty($token)) {
+
+        $resetTokenResponse = $this->get('resetTokenResponse','Survey',$surveyId,"");
+        if(empty($resetTokenResponse)) {
+            if($oSurvey->alloweditaftercompletion == "Y") {
+                $resetTokenResponse = "N";
+            } else {
+                $resetTokenResponse = "Y";
+            }
+        }
+        $bResetTokenResponse = $resetTokenResponse == "Y";
+
+        $oResponse = Response::model($surveyId)->findByPk($responseId);
+        if(empty($oResponse) || empty($oResponse->token)) {
             return;
         }
+        $token = $oResponse->token;
+
         if(substr($token, -strlen($fixedCode)) === $fixedCode){
             return;
         }
@@ -64,7 +101,8 @@ class updateTokenAfterSubmit extends PluginBase {
             return;
         }
         $oToken->token = $token.$fixedCode;
-        if($this->get('resetToken','Survey',$surveyId,1)) {
+
+        if($bResetTokenResponse) {
             $oToken->completed = "N";
             $oToken->usesleft++;
         }
@@ -96,16 +134,19 @@ class updateTokenAfterSubmit extends PluginBase {
         } else {
             $this->log("Update token code {$token} in survey {$surveyId} with success",'info');
         }
-        if($this->get('resetToken','Survey',$surveyId,1)) {
-            $oResponse = Response::model($surveyId)->findByPk($responseId);
+
+        if($bResetTokenResponse) {
             $oResponse->submitdate = null;
-            if(!$oResponse->save()) {
-                $this->log("Unable to update response {$responseId} in survey {$surveyId}",'error');
-                $this->log(sprintf("With error %",CVarDumper::dumpAsString($oResponse->getErrors(), 2, false)),'warning');
-            } else {
-                $this->log("Update response {$responseId} in survey {$surveyId} with success",'info');
-            }
+            $oResponse->lastpage = 1;
         }
+        $oResponse->token = $token.$fixedCode;
+        if(!$oResponse->save()) {
+            $this->log("Unable to update response {$responseId} in survey {$surveyId}",'error');
+            $this->log(sprintf("With error %",CVarDumper::dumpAsString($oResponse->getErrors(), 2, false)),'warning');
+        } else {
+            $this->log("Update response {$responseId} in survey {$surveyId} with success",'info');
+        }
+        return true;
     }
 
     /** @inheritdoc **/
@@ -113,38 +154,67 @@ class updateTokenAfterSubmit extends PluginBase {
     {
         $oEvent = $this->event;
         $surveyId = $oEvent->get('survey');
+        $oSurvey = Survey::model()->findByPk($surveyId);
+        $aSettings = array();
+        if(!$this->_surveySettingsAllowUsage($surveyId)) {
+            $aSettings['warning']= array(
+                'type' => 'info',
+                'content' => "<div class='alert alert-warning warning'>".$this->_translate("Curent settings disallow usage of this plugin: anonymized responses and token-based response persistence must be on")."</div>",
+            );
+        } else {
+            if($oSurvey->autoredirect == "Y") {
+                $aSettings['warning']= array(
+                    'type' => 'info',
+                    'content' => "<div class='alert alert-warning warning'>".$this->_translate("Automatically load URL when survey complete is activated, this disable usage of this plugin.")."</div>",
+                );
+            }
+        }
+        $aSettings = array_merge($aSettings,array(
+            'fixedCode' => array(
+                'type' => 'string',
+                'label' => $this->_translate("Fixed code to add when survey is submitted."),
+                'help' => $this->_translate("This code is added at the end of the token only if it does not end already with this code."),
+                'current' => $this->get('fixedCode','Survey',$surveyId,"")
+            ),
+            'keepTrackOn' => array(
+                'type' => 'select',
+                'options' => $this->_getTokensAttributeList($surveyId),
+                'htmlOptions' => array(
+                    'empty' => $this->_translate("No tracking on token table"),
+                ),
+                'label' => $this->_translate("Where to keep track information about updated token."),
+                'help' => $this->_translate("Data for this token is updated to updateTokenAfterSubmit-X, where X is the number of updating done."),
+                'current'=>$this->get('keepTrackOn','Survey',$surveyId,""),
+            ),
+            'resetTokenResponse' => array(
+                'type' => 'select',
+                'options' => array(
+                    'Y'=>gT("Yes"),
+                    'N'=>gT("No"),
+                ),
+                'htmlOptions' => array(
+                    'empty' => $this->_translate("Automatic"),
+                ),
+                'label' => $this->_translate("Reset token and response to allow editing response. "),
+                'help' => $this->_translate("Token is reset to not completed and response to not submitted.Automatic mean do it if survey don‘t allow edit after completion."),
+                'current'=>$this->get('resetTokenResponse','Survey',$surveyId,""),
+            ),
+            'emailUpdatedTo' => array(
+                'type' => 'string',
+                'label' => $this->_translate("Update email with new value."),
+                'help' => $this->_translate("Usage of expression is allowed and invalid email is saved too."),
+                'current'=>$this->get('emailUpdatedTo','Survey',$surveyId,""),
+            ),
+            'autoRedirect' => array(
+                'type' => 'boolean',
+                'label' => $this->_translate("Automatically load URL after this plugin action."),
+                'help' => $this->_translate("You can‘t use automatically load URL when survey complete from limesurvey core, then if needed activate it here. Warning : no replacement is done on the url. This redirection is done only if token are updated."),
+                'current'=>$this->get('autoRedirect','Survey',$surveyId,0),
+            ),
+        ));
         $oEvent->set("surveysettings.{$this->id}", array(
             'name' => get_class($this),
-            'settings' => array(
-                'fixedCode' => array(
-                    'type' => 'string',
-                    'label' => $this->_translate("Fixed code to add when survey is submitted."),
-                    'help' => $this->_translate("This code is added at the end of the token only if it does not end already with this code."),
-                    'current' => $this->get('fixedCode','Survey',$surveyId,"")
-                ),
-                'keepTrackOn' => array(
-                    'type' => 'select',
-                    'options' => $this->_getTokensAttributeList($surveyId),
-                    'htmlOptions' => array(
-                        'empty' => $this->_translate("No tracking on token table"),
-                    ),
-                    'label' => $this->_translate("Where to keep track information about updated token."),
-                    'help' => $this->_translate("Data for this token is updated to updateTokenAfterSubmit-X, where X is the number of updating done."),
-                    'current'=>$this->get('keepTrackOn','Survey',$surveyId,""),
-                ),
-                'resetToken' => array(
-                    'type' => 'boolean',
-                    'label' => $this->_translate("Reset token and response to allow editing response."),
-                    'help' => $this->_translate("If needed : token is reset to not completed and response to not submitted."),
-                    'current'=>$this->get('resetToken','Survey',$surveyId,1),
-                ),
-                'emailUpdatedTo' => array(
-                    'type' => 'string',
-                    'label' => $this->_translate("Update email with new value."),
-                    'help' => $this->_translate("Usage of expression is allowed and invalid email is saved too."),
-                    'current'=>$this->get('emailUpdatedTo','Survey',$surveyId,""),
-                ),
-            )
+            'settings' => $aSettings
         ));
     }
 
@@ -196,10 +266,24 @@ class updateTokenAfterSubmit extends PluginBase {
     {
         if(!$logDetail && $this->getEvent()) {
             $logDetail = $this->getEvent()->getEventName();
-        } // What to put if no event ?
+        }
+        if(empty($logDetail)) {
+            $logDetail = "unknow";
+        }
         if(intval(Yii::app()->getConfig('versionnumber')) >= 3) {
             parent::log($message, $level);
         }
-        Yii::log($message, $level,'application.plugins.reloadAnyResponse.'.$logDetail);
+        Yii::log($message, $level,'application.plugins.updateTokenAfterSubmit.'.$logDetail);
+    }
+
+    /**
+     * Find if survey settings allow usage of this plugin
+     * @param integer $surveyId
+     * return boolean
+     */
+    private function _surveySettingsAllowUsage($surveyId)
+    {
+        $oSurvey = Survey::model()->findByPk($surveyId);
+        return ($oSurvey->anonymized !="Y" && $oSurvey->tokenanswerspersistence =="Y");
     }
 }
